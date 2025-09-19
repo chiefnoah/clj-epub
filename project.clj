a3ab8aa9dffae6e6bbd55403ed708f09aac17416
Noah Pederson <noah@packetlost.dev>
2025-09-18 21:54:40 -0500

    UPDATE: use deps.edn

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clj-epub "0.2.0"
-  :description "Library for generate EPUB, on Clojure"
-  :url "http://github.com/chiefnoah/clj-epub"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [hiccup "0.3.0"]
-                 [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :repositories {"markdownj" "http://scala-tools.org/repo-releases"})
